3cda97f6c5b97a8fcf1d54a10107bb9debb73def
Kevin Chan <kevin+github@humblefrontiers.com>
2024-09-10 14:35:43 -0400

    Add toy sqlite db

--- index-ws.js
+++ index-ws.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function() {console.log('Server listening on port 3000!')});
 
+process.on('SIGINT', () => {
+   wss.clients.forEach(function each(client) {
+      client.close();
+   });
+
+   server.close(() => shutdownDB());
+})
+
 /** Create websocket **/
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server: server });
@@ -29,8 +37,33 @@ wss.on('connection', function connection(ws) {
       ws.send('Welcome to my server');
    }
 
+   db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`)
+   
    ws.on('close', function close(){
       wss.broadcast(`Current visitors: ${numClients}`);
       console.log('Connection closed!');
    })
 });
+
+/** Create sqlite database **/
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+   db.run(`
+        CREATE TABLE IF NOT EXISTS visitors (
+            count INTEGER,
+            time TEXT
+        )
+   `);
+});
+
+function getCounts() {
+   db.each("SELECT * FROM visitors", (err, row) => console.log(row));
+}
+
+function shutdownDB() {
+   getCounts();
+   console.log('Connection shutdown');
+   db.close();
+}
